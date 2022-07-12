82712b9796eef21e4ad165a43eab6d2a1d73bf49
Rio Ardian <rioriogue@gmail.com>
2022-07-12 08:29:21 +0700

    Init

---
+++ script.js
@@ -0,0 +1,174 @@
+var ourInstance = null;
+var originWidth = 1280;
+var originHeight = 720;
+var scaleToFit = true;
+
+var canvas = document.querySelector("#unity-canvas");
+var container = canvas.parentElement;
+var loadingPanel = document.querySelector("#unity-loading");
+var loadingBar = document.querySelector(".progress-bar2");
+var loadingText = document.querySelector("#unity-loading-text");
+var config = {
+    dataUrl: "Build/Near.data",
+    frameworkUrl: "Build/Near.framework.js",
+    codeUrl: "Build/Near.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "RealityChain",
+    productName: "2dversePreviwerApp",
+    productVersion: "1.0.2",
+};
+container.setAttribute("isportrait", false);
+
+function onResize() {
+    var w;
+    var h;
+
+    if (window.innerWidth > window.innerHeight) {
+        container.setAttribute("isportrait", false);
+    } else {
+        container.setAttribute("isportrait", true);
+    }
+
+    if (scaleToFit) {
+        w = window.innerWidth;
+        h = window.innerHeight;
+        var r = originHeight / originWidth;
+
+        if (container.getAttribute("isportrait").toLowerCase() == "true") {
+            r = originWidth / originHeight;
+        }
+
+        if (w * r > window.innerHeight) {
+            w = Math.min(w, Math.ceil(h / r));
+        }
+        h = Math.floor(w * r);
+    } else {
+        if (container.getAttribute("isportrait").toLowerCase() == "true") {
+            w = originHeight;
+            h = originWidth;
+        } else {
+            w = originWidth;
+            h = originHeight;
+        }
+    }
+
+    container.style.width = canvas.style.width = w + "px";
+    container.style.height = canvas.style.height = h + "px";
+    container.style.top = Math.floor((window.innerHeight - h) / 2) + "px";
+    container.style.left = Math.floor((window.innerWidth - w) / 2) + "px";
+}
+
+function toggleOrientation(isportrait) {
+    container.setAttribute("isportrait", isportrait);
+    onResize();
+}
+
+function callUnityResize(objName, objCallback) {
+    onResize();
+    ourInstance.Module.SendMessage(objName, objCallback, container.getAttribute("isportrait").toLowerCase());
+}
+
+function openTwitch(targetChannel, audioOnly) {
+    var options = {
+        width: 400,
+        height: 300,
+        channel: targetChannel,
+        // only needed if your site is also embedded on embed.example.com and othersite.example.com
+        parent: ["embed.example.com", "othersite.example.com"]
+    };
+    var player = new Twitch.Player("div-embed", options);
+    player.setVolume(0.5);
+
+	if(audioOnly == "False"){
+		document.getElementById("div-embed").style.display = "block";
+		dragElement(document.getElementById("div-embed"));
+	}
+}
+
+function closeEmbed() {
+    document.getElementById("div-embed").style.display = "none";
+    document.getElementById("div-embed").innerHTML = "<div id='div-embed-header'><span>Click here to move</span><button class='div-close' type='button' onclick='closeEmbed()'>×</button></div><div id='player'></div>";
+}
+
+function openYoutube(targetChannel, audioOnly) {
+	var player = new YT.Player('player', {
+          width: '400',
+          height: '300',
+          videoId: targetChannel,
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady
+          }
+        });
+		
+	if(audioOnly == "False"){
+		document.getElementById("div-embed").style.display = "block";
+		dragElement(document.getElementById("div-embed"));
+	}
+}
+
+function onPlayerReady(event) {
+	event.target.playVideo();
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + "-header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+createUnityInstance(canvas, config, (progress) => {
+    var p = Math.ceil(100 * progress);
+    loadingBar.style.width = p + "%";
+	
+	if(p < 90){
+		loadingText.innerHTML = "Loading " + p + "%";
+	}
+	else {
+		loadingText.innerHTML = "Preparing " + p + "%";
+	}
+}).then(function(instance) {
+    loadingPanel.style.display = "none";
+    ourInstance = instance;
+    callUnityResize();
+});
